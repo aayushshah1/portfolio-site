e90a5b3f2c142d67e23abdcec9d768d648834932
aayushshah1 <126041880+aayushshah1@users.noreply.github.com>
2025-04-15 13:59:47 +0530

    Information update: Updates new information.

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ function changeLanguage() {
     }, 3000);
 }
 
+// Show or hide the button based on scroll position
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.querySelector(".back-to-top").style.display = "block";
+    } else {
+        document.querySelector(".back-to-top").style.display = "none";
+    }
+};
+
+// Scroll to the top of the page
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // Call the function when the page loads
 window.onload = changeLanguage;
+
